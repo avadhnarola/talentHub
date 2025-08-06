087d81140297961878d85a6e28815755d609202d
avadh <avadhnarola2515@gmail.com>
2025-08-06 17:19:48 +0530

    new

--- Database Backups/talenthub.sql
+++ Database Backups/talenthub.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 06, 2025 at 12:25 PM
+-- Generation Time: Aug 06, 2025 at 01:48 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `e-learning`
+-- Database: `talenthub`
 --
 
 -- --------------------------------------------------------
